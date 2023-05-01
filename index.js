c5c85d3c4e326fb5f2f151934780bb76fd99ded2
Bennett Rosenthal <bsrosenthal101@gmail.com>
2023-05-01 12:04:34 -0700

    added bg image

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 counter = 0
 function switchImage(element) {
     if (counter == 0) {
-        element.src = "on_switch.png";
-        var fade = document.getElementById("logo");
-        var opacity = 0;
-        var intervalID = setInterval(function() {
-            if (opacity < 1) {
-                opacity = opacity + 0.1
-                fade.style.opacity = opacity;
-            } else {
-                clearInterval(intervalID);
-            }
-        }, 15);
-        function test1() {
-            var opacity2 = 1;
-            var interval = setInterval(function() {
-                if (opacity2 > 0) {
-                    opacity2 = opacity2 - 0.1
-                    element.style.opacity = opacity2;
-                } else {
-                    clearInterval(interval);
-                }
-            }, 30);
-            counter = 1;
-        }
-        
-        setTimeout(() => { test1() }, 1000);
+        fadeInLogo()
+        setTimeout(() => { fadeOutSwitch() }, 500);
+        setTimeout(() => { fadeInBG() }, 1500);
     }
 }
 
+function fadeInLogo() {
+    document.getElementById("switch").src = "on_switch.png";
+    var logo = document.getElementById("logo");
+    var opacity = 0;
+    var intervalID = setInterval(function() {
+        if (opacity < 1) {
+            opacity = opacity + 0.1
+            logo.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 15);
+}
+
+function fadeOutSwitch() {
+    var light_switch = document.getElementById("switch");
+    var opacity = 1;
+    var interval = setInterval(function() {
+        if (opacity > 0) {
+            opacity = opacity - 0.1
+            light_switch.style.opacity = opacity;
+        } else {
+            clearInterval(interval);
+        }
+    }, 30);
+    counter = 1;
+}
+
+function fadeInBG() {
+    var bg = document.getElementById("bg");
+    var opacity = 0;
+    var intervalID = setInterval(function() {
+        if (opacity < 1) {
+            opacity = opacity + 0.1
+            logo.style.opacity = opacity;
+            bg.style.opacity = opacity;
+        } else {
+            clearInterval(intervalID);
+        }
+    }, 15);
+}
 
 function sleep(milliseconds) {
     const date = Date.now();
@@ -36,4 +55,4 @@ function sleep(milliseconds) {
     do {
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
-  }
+}
